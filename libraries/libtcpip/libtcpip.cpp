86e4531aeaf2c32f5161063657450dbdcaa29a6e
gozfree <ustcgzf@gmail.com>
2012-06-03 18:25:20 +0800

    update libtcpip for linux version

--- libraries/libtcpip/libtcpip.cpp
+++ libraries/libtcpip/libtcpip.cpp
@@ -1,9 +1,10 @@
 #include "libtcpip.h"
 #define ENABLE	1
 #define DISABLE	0
+
 int socket_init()
 {
-#ifdef WIN32
+#ifdef _WIN32_
 	WSADATA wsaData;
 	int ret = WSAStartup(MAKEWORD(2, 2), &wsaData);
 	if(ret != NO_ERROR)
@@ -12,10 +13,19 @@ int socket_init()
 		WSACleanup();
 		return -1;
 	}
-#elif LINUX
+#elif _LINUX_
 
 #endif
 	return 0;
+}
+void socket_close(SOCKET* s)
+{
+#ifdef _WIN32_
+	closesocket(*s);
+#elif _LINUX_
+	close(*s);
+#endif
+
 }
 int tcp_socket(SOCKET* s)
 {
@@ -27,10 +37,13 @@ int tcp_socket(SOCKET* s)
 		WSACleanup();
 		return -1;
 	}
-
+#ifdef _WIN32_
 	if(ioctlsocket(*s, FIONBIO, &noblock) == SOCKET_ERROR)
+#elif _LINUX_
+	if(ioctl(*s, FIONBIO, &noblock) == SOCKET_ERROR)
+#endif
 	{
-		closesocket(*s);
+		socket_close(s);
 		fprintf(stdout, "Error at ioctlsocket(): %ld\n", WSAGetLastError());
 		WSACleanup();
 		return -1;
@@ -38,10 +51,10 @@ int tcp_socket(SOCKET* s)
 	return 0;
 }
 
-int tcp_bind(SOCKET* s, u_short port)
+int tcp_bind(SOCKET* s, u_short port, char* ip = LOCAL_HOST)
 {
 	struct sockaddr_in addr;
-	struct hostent* host = (struct hostent*)gethostbyname("");
+	struct hostent* host = (struct hostent*)gethostbyname(ip);
 	char* localhost = inet_ntoa(*(struct in_addr*)*host->h_addr_list);
 	if(*s == NULL || port < 0 || port > 65535)
 	{
@@ -51,12 +64,13 @@ int tcp_bind(SOCKET* s, u_short port)
 
 
 	addr.sin_family = AF_INET;
+//	addr.sin_addr.s_addr = htonl(INADDR_ANY);
 	addr.sin_addr.s_addr = inet_addr(localhost);
 	addr.sin_port = htons(port);
-
+//	bzero(&(addr.sin_zero), 8);
 	if(bind(*s, (struct sockaddr*)&addr, sizeof(addr)) == SOCKET_ERROR)
 	{
-		closesocket(*s);
+		socket_close(s);
 		fprintf(stdout, "Error at bind(): %ld\n", WSAGetLastError());
 		WSACleanup();
 		return -1;
@@ -69,18 +83,18 @@ int tcp_listen(SOCKET* s)
 	int iOptVal;
 	int iOptLen = sizeof(int);
 
-	if(getsockopt(*s, SOL_SOCKET, SO_KEEPALIVE, (char*)&iOptVal, &iOptLen) != SOCKET_ERROR)
+	if(getsockopt(*s, SOL_SOCKET, SO_KEEPALIVE, (char*)&iOptVal,(socklen_t*)&iOptLen) != SOCKET_ERROR)
 	{
 		fprintf(stdout, "SO_KEEPALIVE Value: %ld\n", iOptVal);
 	}
 	if(listen(*s, SOMAXCONN) == SOCKET_ERROR)
 	{
-		closesocket(*s);
+		socket_close(s);
 		fprintf(stdout, "Error at listen(): %ld\n", WSAGetLastError());
 		WSACleanup();
 		return -1;
 	}
-	if(getsockopt(*s, SOL_SOCKET, SO_KEEPALIVE, (char*)&iOptVal, &iOptLen) != SOCKET_ERROR)
+	if(getsockopt(*s, SOL_SOCKET, SO_KEEPALIVE, (char*)&iOptVal, (socklen_t*)&iOptLen) != SOCKET_ERROR)
 	{
 		fprintf(stdout, "SO_KEEPALIVE Value: %ld\n", iOptVal);
 	}
@@ -93,11 +107,11 @@ int tcp_accept(SOCKET* s, struct sockaddr_in* premote, SOCKET* as)
 	char* remote = inet_ntoa(premote->sin_addr);
 	memset(premote, 0, sizeof(struct sockaddr_in));
 
-	*as = accept(*s, (struct sockaddr*)premote, &addrlen);
+	*as = accept(*s, (struct sockaddr*)premote, (socklen_t*)&addrlen);
 	if(*as == INVALID_SOCKET)
 	{
+		socket_close(s);
 		fprintf(stdout, "accept failed: %d\n", WSAGetLastError());
-		closesocket(*s);
 		WSACleanup();
 		return -1;
 	}
@@ -106,11 +120,11 @@ int tcp_accept(SOCKET* s, struct sockaddr_in* premote, SOCKET* as)
 	return 0;
 }
 
-int tcp_connect(SOCKET* s, u_long remote, u_short port)
+int tcp_connect(SOCKET* s, char* ip, u_short port)
 {
 	struct sockaddr_in addr;
 	addr.sin_family = AF_INET;
-	addr.sin_addr.s_addr = remote;
+	addr.sin_addr.s_addr = inet_addr(ip);
 	addr.sin_port = htons(port);
 
 	connect(*s, (struct sockaddr*)&addr, sizeof(addr));
@@ -123,8 +137,8 @@ int tcp_send(SOCKET* s, void* buf)
 	int ret = send(*s, (char*)buf, strlen((char*)buf), 0);
 	if(ret == SOCKET_ERROR)
 	{
+		socket_close(s);
 		fprintf(stdout, "send failed: %d\n", WSAGetLastError());
-		closesocket(*s);
 		WSACleanup();
 		return -1;
 	}
@@ -137,7 +151,7 @@ int tcp_recv(SOCKET* s, void* buf)
 	if(ret == SOCKET_ERROR)
 	{
 		fprintf(stdout, "recv failed: %d\n", WSAGetLastError());
-		closesocket(*s);
+		socket_close(s);
 		WSACleanup();
 		return -1;
 	}
@@ -150,6 +164,7 @@ int udp_socket(SOCKET* s)
 	if(*s == INVALID_SOCKET)
 	{
 		fprintf(stdout, "Error at socket(): %ld\n", WSAGetLastError());
+		socket_close(s);
 		WSACleanup();
 		return -1;
 	}
@@ -167,7 +182,7 @@ int udp_sendto(SOCKET* s, void* buf, u_long remote, u_short port)
 	if(ret == SOCKET_ERROR)
 	{
 		fprintf(stdout, "sendto failed: %d\n", WSAGetLastError());
-		closesocket(*s);
+		socket_close(s);
 		WSACleanup();
 		return -1;
 	}
@@ -184,13 +199,13 @@ int udp_recvfrom(SOCKET* s, char* buf, char* ip, u_short port)
 	addr.sin_port = htons(port);
 	addrsize = sizeof(addr);
 
-	ret = recvfrom(*s, (char*)buf, UDP_MSG_MAX_LEN, 0, (struct sockaddr*)&addr, &addrsize);
+	ret = recvfrom(*s, (char*)buf, UDP_MSG_MAX_LEN, 0, (struct sockaddr*)&addr, (socklen_t*)&addrsize);
 	if(ret == SOCKET_ERROR)
 	{
 		fprintf(stdout, "recvfrom failed: %d\n", WSAGetLastError());
 		if(WSAGetLastError() != 10040)
 		{
-			closesocket(*s);
+			socket_close(s);
 			WSACleanup();
 			return -1;
 		}
@@ -234,4 +249,47 @@ int set_cb_msg(cb_msg_callback cbmsg)
 
 //	ret = udp_recvfrom(SOCKET* s, char* buf, char* ip, u_short port, int sendrecv);
 	printf("hello \n");
+}
+
+int tcp_server_start(u_short server_port)
+{
+	int i = 0;
+	char* recv_buf;
+	recv_buf = (char*)malloc(1024);
+
+	SOCKET sock, accept_sock;
+	struct sockaddr_in remote_ip;
+
+	socket_init();
+	tcp_socket(&sock);
+	tcp_bind(&sock, server_port);
+	tcp_listen(&sock);
+	while(1)
+	{
+		tcp_accept(&sock, &remote_ip, &accept_sock); 
+		tcp_recv(&accept_sock, (void*)recv_buf); 
+		printf("tcp client connect times = %d\n", i);
+		printf("tcp receive buffer: %s\n", (char*)recv_buf);
+		i++;
+	}
+}
+int tcp_client_start(char* ip, u_short port, char* send_buf = NULL)
+{
+	if(send_buf == NULL)
+	{
+		send_buf = (char*)malloc(1024);
+		memset(send_buf, 0x01020304, 1024);
+	}
+	SOCKET sock;
+	socket_init();
+	tcp_socket(&sock);
+	tcp_connect(&sock, ip, port);
+	tcp_send(&sock, (void*)&send_buf);
+
+}
+int main()
+{
+//	tcp_server_start(5060);
+	tcp_client_start("127.0.0.1", 5060);
+	return 0;
 }
--- libraries/libtcpip/libtcpip.h
+++ libraries/libtcpip/libtcpip.h
@@ -5,18 +5,22 @@
 #include <stdlib.h>
 #include <stddef.h>
 
-#ifdef WIN32
+#ifdef _WIN32_
 #include <WinSock2.h>
 #include <ws2tcpip.h>
 
 #pragma comment(lib, "ws2_32")
 
-#elif LINUX
+#elif _LINUX_
 #include <sys/types.h>
+#include <string.h>
 
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
 #include <netdb.h>
 #include <asm/ioctls.h>
 #include <errno.h>
@@ -27,6 +31,7 @@ typedef int SOCKET;
 #define WSAGetLastError()	0
 #endif
 
+#define LOCAL_HOST	"127.0.0.1"
 
 #ifdef __cplusplus
 extern "C" {
@@ -36,26 +41,27 @@ extern "C" {
 #define TCP_MSG_MAX_LEN		1452
 
 int socket_init();
-int socket_close();
+void socket_close(SOCKET* s);
 int socket_shutdown();
 
-	int tcp_socket(SOCKET* s);
-	int tcp_bind(SOCKET* s, u_short port);
-	int tcp_listen(SOCKET* s);
-	int tcp_accept(SOCKET* s, struct sockaddr_in* preomte_addr, SOCKET* rs);
-	int tcp_connect(SOCKET* s, u_long remote, u_short port);
-	int tcp_send(SOCKET* s, void* buf);
-	int tcp_recv(SOCKET* s, void* buf);
+int tcp_socket(SOCKET* s);
+int tcp_bind(SOCKET* s, u_short port, char* ip);
+int tcp_listen(SOCKET* s);
+int tcp_accept(SOCKET* s, struct sockaddr_in* preomte_addr, SOCKET* rs);
+int tcp_connect(SOCKET* s, char* ip, u_short port);
+int tcp_send(SOCKET* s, void* buf);
+int tcp_recv(SOCKET* s, void* buf);
 
-	int udp_socket(SOCKET* s);
-	int udp_sendto(SOCKET* s, void* buf, u_long remote, u_short port);
-	int udp_recvfrom(SOCKET* s, char* buf, char* ip, u_short port);
+int tcp_server_start(u_short server_port);
+int tcp_client_start(char* ip, u_short port, char* send_buf);
+int udp_sendto(SOCKET* s, void* buf, u_long remote, u_short port);
+int udp_recvfrom(SOCKET* s, char* buf, char* ip, u_short port);
 
-	int get_addrinfo();
+int get_addrinfo();
 
 typedef void (*cb_msg_callback) (SOCKET* s, int sendrecv);
 
-	int set_cb_msg(cb_msg_callback cbmsg);
+int set_cb_msg(cb_msg_callback cbmsg);
 #define udp_bind(s, port) tcp_bind(s, port)
 
 
