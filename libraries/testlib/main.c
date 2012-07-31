1f31529e5fd1837e68844a4454c1be9bc9ec1d69
root <root@ubuntu.(none)>
2012-08-01 01:03:04 +0800

    update testlib

--- libraries/testlib/main.c
+++ libraries/testlib/main.c
@@ -1,9 +1,37 @@
 
 #include <stdlib.h>
 
+enum PRINT_LEVEL {
+	LOG = 0,
+	DBG,
+	WAR,
+	ERR
+};
+
+#define	PROMPT		"testlib>"
+
+void dbg_printf(SIGN32 logLevel, const char* string, ...)
+{
+	static char dbg_buf[256];
+	va_list args;
+	
+	va_start(args, string);
+	vsprintf(dbg_buf, string, args);
+	va_end(args);
+}
+
+void show_prompt(char* prompt)
+{
+	dbg_printf(LOG, "\n%s", prompt);
+}
 
 int main()
 {
+
+	while (1) {
+		show_prompt(PROMPT);
+		
+		}
 	
 	return 0;
 }
