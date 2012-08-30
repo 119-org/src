7d0ba19a7e3b847ccfba7ba97e2dda6f2fbef259
gozfree <ustcgzf@gmail.com>
2012-08-31 00:28:31 +0800

    add main.cpp

---
+++ libraries/libtcpip/main.cpp
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+#include "libtcpip.h"
+
+int main()
+{
+//	cb_msg_callback cbmsg;
+//	cbmsg = (cb_msg_callback)udp_recvfrom;
+//	tcp_server_start(5060);
+//	tcp_client_start("127.0.0.1", 5060);
+//	udp_server_start(5060);
+//	udp_client_start("127.0.0.1", 5060);
+//	cb_udp_server_start(cbmsg);
+	return 0;
+}
